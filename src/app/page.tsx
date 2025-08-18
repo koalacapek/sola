a20acc223c8bb503f773da8443387586363511bb
koalacapek <hanskr7882@gmail.com>
2025-08-18 15:29:36 +1000

    add auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 
 import { LatestPost } from "@/app/_components/post";
-import { auth } from "@/server/auth";
+import { auth, signIn } from "@/server/auth";
 import { api, HydrateClient } from "@/trpc/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
 
   if (session?.user) {
     void api.post.getLatest.prefetch();
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,9 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@/server/db";
+import { env } from "@/env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -31,8 +32,12 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
+  secret: env.AUTH_SECRET,
   providers: [
-    DiscordProvider,
+    GoogleProvider({
+      clientId: env.AUTH_GOOGLE_CLIENT_ID as string,
+      clientSecret: env.AUTH_GOOGLE_CLIENT_SECRET as string,
+    }),
     /**
      * ...add more providers here.
      *
